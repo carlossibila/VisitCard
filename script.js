ab8f8dc7eb2afdbd44e3480c7a062ec9465ad82e
Carlos Henrique Sibila <carlos.sibila@gmail.com>
2026-01-06 16:55:52 -0300

    new address upgrade

---
+++ script.js
@@ -0,0 +1,23 @@
+// Platform-specific map routing
+function openMaps() {
+    const address = "Rua Quadrangular 3622, Primo Meneghetti, Franca, SP, 14403-220";
+    const encodedAddress = encodeURIComponent(address);
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    let mapUrl;
+
+    // iOS - Open Apple Maps app
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        mapUrl = `maps://maps.apple.com/?q=${encodedAddress}`;
+    }
+    // Android - Open Google Maps app
+    else if (/android/i.test(userAgent)) {
+        mapUrl = `geo:0,0?q=${encodedAddress}`;
+    }
+    // Desktop/Other - Open Google Maps web
+    else {
+        mapUrl = `https://www.google.com/maps/search/?api=1&query=${encodedAddress}`;
+    }
+
+    window.location.href = mapUrl;
+}
